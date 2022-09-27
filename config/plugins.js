8685f1d4e5fd7c36519b6959ababe8476863c18c
pedro-phd <pedro_dominici@outlook.com>
2022-09-27 00:13:40 -0300

    configure plugin

--- config/plugins.js
+++ config/plugins.js
@@ -26,4 +26,14 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: '/api/',
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      }
+    }
+  },
 });
